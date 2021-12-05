35fd51b1351e87b89b3d2a89d5e12cb03c18a74a
thiago cezar <thiagocezarns@gmail.com>
2021-12-04 22:45:12 -0300

    finalizando lista duplamente encadeada

--- listaDupla/alunos.rb
+++
@@ -1,9 +0,0 @@
-require 'faker'
-
-File.open("alunos.txt", "w") do |file|
-  i = 0
-  while(i < 1_000_010) do
-    file.write("#{Faker::Name.first_name} #{Faker::Number.number(digits: 7)}\n")
-    i += 1
-  end
-end
--- listaDupla/main.c
+++ listaDupla/main.c
@@ -9,29 +9,45 @@ struct aluno al;
 
 int main()
 {
-    FILE *file;
-   file = fopen("alunos.txt", "r");
-
     minha_lista = criar_lista();
     printf("Lista criada! \n");
 
-    for(int i=0; i<10000; i++) {
-        char name[20];
-        int id_matricula;
-        fscanf(file, "%s %d", name, &id_matricula);
+    al.matricula = 1;
+    strcpy(al.nome,"Teste Joao");
+    al.n1 = 7.5;
+    al.n2 = 8.2;
+    al.n3 = 7.4;
+    inserir_lista_ordenada(minha_lista,al);
 
-        al.matricula = id_matricula;
-        strcpy(al.nome, name);
-        al.n1 = 6.5;
-        al.n2 = 8.2;
-        al.n3 = 5.4;
-        inserir_lista_ordenada(minha_lista,al);
-    }
-    fclose(file);
+    al.matricula = 4;
+    strcpy(al.nome,"Teste Maria");
+    al.n1 = 7.5;
+    al.n2 = 9.2;
+    al.n3 = 9.4;
+    inserir_lista_ordenada(minha_lista,al);
 
+    al.matricula = 2;
+    strcpy(al.nome,"Novo Joao");
+    al.n1 = 6.5;
+    al.n2 = 8.2;
+    al.n3 = 5.4;
+    inserir_lista_ordenada(minha_lista,al);
+
+    al.matricula = 3;
+    strcpy(al.nome,"Novo Maria");
+    al.n1 = 6.5;
+    al.n2 = 8.2;
+    al.n3 = 5.4;
+    inserir_lista_ordenada(minha_lista,al);
+    
+    imprimir_lista(minha_lista);
+    printf("\n\n----------");
+
+    remover_lista_inicio(minha_lista);
+    remover_lista_final(minha_lista);
     imprimir_lista(minha_lista);
 
     liberar_lista(minha_lista);
 
-    printf("A lista foi liberada!");
+    printf("\nA lista foi liberada!\n");
 }
